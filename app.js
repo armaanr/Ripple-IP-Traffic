337dbcc00b2b137d0005ca33e24f59bbf439d173
armaanr <rai.armaan91@gmail.com>
2016-09-18 16:33:01 -0500

    adding database filler

--- app.js
+++ app.js
@@ -21,3 +21,23 @@ app.get('/', function(req, res) {
 app.listen(port, function() {
     console.log('Our app is running on http://localhost:' + port);
 });
+
+//connects to postgres db
+var pg = require('pg');
+pg.defaults.ssl = true;
+
+app.get('/db', function (request, response) {
+    pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+        client.query('SELECT * FROM test_table', function(err, result) {
+            done();
+            if (err)
+            { console.error(err); response.send("Error " + err); }
+            else
+            { response.render('pages/db', {results: result.rows} ); }
+        });
+    });
+});
+
+
+
+
---
+++ fillData.js
@@ -0,0 +1,74 @@
+var pg = require('pg');
+pg.defaults.poolIdleTimeout = 2000;
+//pg.defaults.ssl = true;
+var connectionString = {
+    user: 'zziybtldxgdpqt',
+    password: '1alMVnpHdShvbCMgbn9AGJNxND',
+    database: 'd3etic8hcd3h7c',
+    host: 'ec2-50-17-209-1.compute-1.amazonaws.com',
+    port: '5432',
+    max: 19,
+    idleTimeoutMillis: 2000,
+    ssl: true
+};
+
+//var client = new pg.Client(connectionString);
+console.log(connectionString);
+
+var LineByLineReader = require('line-by-line'),
+    lr = new LineByLineReader('../../logs.txt');
+
+lr.on('error', function (err) {
+    console.error(err);
+    // 'err' contains error object
+});
+
+lr.on('line', function (line) {
+    // pause emitting of lines...
+    lr.pause();
+
+    var tokens = line.split(" ");
+
+    var date = tokens[0];
+    var time = tokens[1].substr(0,15);
+    var timeStamp = date + " " + time;
+    var ip = tokens[6].substr(5,12);
+
+    console.log(timeStamp + " ip address => "+ ip);
+    /*client.connect();
+
+    var query = client.query("INSERT into ripple values (TIMESTAMP '"+timeStamp+"','"+ip+"');");
+
+    query.on('row', function(row) {
+        console.log(row);
+    });
+
+    query.on('end', function() {
+        client.end();
+    });*/
+
+    pg.connect(connectionString, function(err, client) {
+        if (err) throw err;
+        console.log('Connected to postgres! Getting schemas...');
+
+        var query = client.query("INSERT into ripple values (TIMESTAMP '"+timeStamp+"','"+ip+"');");
+
+        query.on('row', function(row) {
+            console.log(row);
+        });
+
+
+    });
+
+
+    // ...do your asynchronous line processing..
+    setTimeout(function () {
+
+        // ...and continue emitting lines.
+        lr.resume();
+    }, 2000);
+});
+
+lr.on('end', function () {
+    // All lines are read, file is closed now.
+});
