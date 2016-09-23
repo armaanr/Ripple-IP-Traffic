570d2d366dea2f6298196a8df4d7097a320bdfa0
armaanr <rai.armaan91@gmail.com>
2016-09-22 23:36:10 -0500

    got client server exchange working

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 var express = require('express');
+var http = require('http');
 var app = express();
 var pg = require('pg');
 
 // set the port of our application
 // process.env.PORT lets the port be set by Heroku
-var port = process.env.PORT || 8082;
+var port = process.env.PORT || 8080;
 
 var connectionString = {
     user: 'zziybtldxgdpqt',
@@ -12,8 +13,21 @@ var connectionString = {
     database: 'd3etic8hcd3h7c',
     host: 'ec2-50-17-209-1.compute-1.amazonaws.com',
     port: '5432',
+    timeout: 10000,
     ssl: true
 };
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// make express look in the public directory for assets (public/js/img)
+app.use(express.static(__dirname + '/public'));
+
+// set the home page route
+app.get('/', function(req, res) {
+    // ejs render automatically looks in the views folder
+    res.render('index',{'ips':ips});
+});
 
 var ips = [];
 
@@ -34,24 +48,60 @@ pg.connect(connectionString, function(err, client) {
 
     query.on('end', function(row){
 
-        // set the view engine to ejs
-        app.set('view engine', 'ejs');
-
-        // make express look in the public directory for assets (public/js/img)
-        app.use(express.static(__dirname + '/public'));
-
-        // set the home page route
-        app.get('/', function(req, res) {
-
-            // ejs render automatically looks in the views folder
-            res.render('index',{'ips':ips});
-        });
-
         app.listen(port, function() {
             console.log('Our app is running on http://localhost:' + port);
         });
 
     });
+
+});
+
+app.get('/data', function(req, res) {
+
+    console.log("connected to other get");
+    var searchIp = req.param('ip');
+    console.log("new ip =>"+ searchIp);
+
+    var reqPerSec = {};
+
+    pg.connect(connectionString, function(err, client) {
+
+        console.log('Connected to postgres! Getting schemas...');
+
+        var query = client.query("SELECT * from ripple where ip = inet '"+searchIp+"';");
+
+        query.on('row', function(row) {
+
+
+
+            if(row['timestamp'] in reqPerSec){
+                var temp = reqPerSec[row['timestamp']];
+                reqPerSec[row['timestamp']] = temp + 1;
+            }
+            else{
+                reqPerSec[row['timestamp']] = 1 ;
+            }
+
+        });
+
+        query.on('end', function(){
+
+            var newIps = [];
+
+            Object.keys(reqPerSec).forEach(function (element, index) {
+                var curr = [element, reqPerSec[element]];
+                newIps.push(curr);
+            });
+
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+
+            res.send(JSON.stringify(newIps.reverse()));
+        });
+
+    });
+
+    // ejs render automatically looks in the views folder
 
 });
 
--- public/js/graph.js
+++ public/js/graph.js
@@ -1,60 +1,99 @@
-$(function () {
-    $.getJSON('https://www.highcharts.com/samples/data/jsonp.php?filename=usdeur.json&callback=?', function (data) {
+var ipButtons = document.getElementsByTagName('li');
 
-        $('#graph').highcharts({
-            chart: {
-                zoomType: 'x'
-            },
+window.onload = function () {
+
+    console.log(ipButtons[0].getAttribute('name'));
+
+    for (var i = 0; i < ipButtons.length; i++) {
+        ipButtons[i].addEventListener('click', getData, false);
+    }
+}
+
+
+function getData(){
+
+    var dataRequest = new XMLHttpRequest();
+    var ip = this.getAttribute('name');
+    var url = "http://localhost:8080/data?ip="+ip+"";
+
+    dataRequest.onreadystatechange = function() {
+        var status = dataRequest.status;
+        var state = dataRequest.readyState;
+        if (state == 4 && status == 200) {
+            var info = JSON.parse(dataRequest.responseText);
+            console.log(info);
+            graphGenerator(info);
+        }
+
+    };
+
+    dataRequest.open("get", url, true);
+    dataRequest.send();
+}
+
+function graphGenerator(data) {
+
+    console.log(data);
+
+    $('#graph').highcharts({
+        chart: {
+            zoomType: 'x'
+        },
+        title: {
+            text: 'Requests per second'
+        },
+        subtitle: {
+            text: document.ontouchstart === undefined ?
+                'Click and drag in the plot area to zoom in' : 'Pinch the chart to zoom in'
+        },
+        xAxis: {
+            type: 'datetime',
+            dateTimeLabelFormats: {
+                second: '%H:%M:%S',
+                minute: '%H:%M',
+                hour: '%H:%M',
+                day: '%e. %b'
+            }
+        },
+        yAxis: {
             title: {
-                text: 'USD to EUR exchange rate over time'
-            },
-            subtitle: {
-                text: document.ontouchstart === undefined ?
-                    'Click and drag in the plot area to zoom in' : 'Pinch the chart to zoom in'
-            },
-            xAxis: {
-                type: 'datetime'
-            },
-            yAxis: {
-                title: {
-                    text: 'Exchange rate'
-                }
-            },
-            legend: {
-                enabled: false
-            },
-            plotOptions: {
-                area: {
-                    fillColor: {
-                        linearGradient: {
-                            x1: 0,
-                            y1: 0,
-                            x2: 0,
-                            y2: 1
-                        },
-                        stops: [
-                            [0, Highcharts.getOptions().colors[0]],
-                            [1, Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
-                        ]
+                text: 'Requests'
+            }
+        },
+        legend: {
+            enabled: false
+        },
+        plotOptions: {
+            area: {
+                fillColor: {
+                    linearGradient: {
+                        x1: 0,
+                        y1: 0,
+                        x2: 0,
+                        y2: 1
                     },
-                    marker: {
-                        radius: 2
-                    },
-                    lineWidth: 1,
-                    states: {
-                        hover: {
-                            lineWidth: 1
-                        }
-                    },
-                    threshold: null
-                }
-            },
+                    stops: [
+                        [0, Highcharts.getOptions().colors[0]],
+                        [1, Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
+                    ]
+                },
+                marker: {
+                    radius: 2
+                },
+                lineWidth: 1,
+                states: {
+                    hover: {
+                        lineWidth: 1
+                    }
+                },
+                threshold: null
+            }
+        },
 
-            series: [{
-                type: 'area',
-                name: 'USD to EUR',
-                data: data
-            }]
-        });
+        series: [{
+            type: 'area',
+            name: 'Requests per second',
+            data: data
+        }]
     });
-});
+};
